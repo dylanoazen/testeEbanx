fcb0100e08fd571829c498a41f0a7679c6eac6c8
Dylan Oazen <dylanoazen@hotmail.com>
2024-06-01 01:49:59 -0300

    correção nome de var e estrutura

--- testeApi.php
+++ testeApi.php
@@ -1,63 +1,62 @@
 <?php  
 
-$acconts = [];  
+$accounts = [];  
 
-    if($_SERVER['REQUEST_METHOD'] === 'POST' and $_SERVER['REQUEST_URI'] === '/reset'){
-        $acconts = [];     
-        http_response_code(200);     
-        exit(); 
-    }  
+if($_SERVER['REQUEST_METHOD'] === 'POST' and $_SERVER['REQUEST_URI'] === '/reset'){
+    $accounts = [];     
+    http_response_code(200);     
+    exit(); 
+}  
 
-    if($_SERVER['REQUEST_METHOD'] === 'GET' and $_SERVER['REQUEST_URI'] === '/balance'){     
-        $accont_id = $_GET['account_id'] ?? null;     
-        if(isset($accounts[$account_id])){         
-            http_response_code(200);         
-            echo json_encode($acconts[$accont_id]);     
-        }else {     
-            http_response_code(404);    
-        }     
-        exit(); 
-    } 
-        
-    if($_SERVER['REQUEST_METHOD'] === 'POST' and $_SERVER['REQUEST_URI'] === '/event'){     
-        $data = json_decode(file_get_contents('php://input'), true);     
-        if ($data['type'] === 'deposit') {         
-            $destination = $data['destination'];         
-            $amout = $data['amount'];
-            if(!isset($accounts[$destination])) {
-                $acconts[$destination] = ['id' => $destination, 'balance' => $amout];
-            }else{
-                $acconts[$destination]['balance'] += $amout;
-            }
-            http_response_code(201);
-            echo json_encode(['origin' => $acconts[$origin]]);
-        } 
-    }elseif ($data['type'] === 'withdraw'){
+if($_SERVER['REQUEST_METHOD'] === 'GET' and $_SERVER['REQUEST_URI'] === '/balance'){     
+    $account_id = $_GET['account_id'] ?? null;     
+    if(isset($accounts[$account_id])){         
+        http_response_code(200);         
+        echo json_encode($accounts[$account_id]);     
+    }else{     
+        http_response_code(404);    
+    }     
+    exit(); 
+} 
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' and $_SERVER['REQUEST_URI'] === '/event'){     
+    $data = json_decode(file_get_contents('php://input'), true);     
+    if ($data['type'] === 'deposit') {         
+        $destination = $data['destination'];         
+        $amount = $data['amount'];
+        if(!isset($accounts[$destination])) {
+            $accounts[$destination] = ['id' => $destination, 'balance' => $amount];
+        }else {
+            $accounts[$destination]['balance'] += $amount;
+        }
+        http_response_code(201);
+        echo json_encode(['destination' => $accounts[$destination]]);
+    }elseif ($data['type'] === 'withdraw') {
         $origin = $data['origin'];
-        $amout = $data['amout'];
-        if(!isset($acconts[$origin]) or $acconts[$origin]['balance']< $amout) {
+        $amount = $data['amount'];
+        if(!isset($accounts[$origin]) or $accounts[$origin]['balance'] < $amount){
             http_response_code(404);
         }else{
-        $acconts[$origin]['balance'] -= $amout;
-        http_response_code(201);
-        echo json_encode(['origin' => $acconts[$origin]]);
+            $accounts[$origin]['balance'] -= $amount;
+            http_response_code(201);
+            echo json_encode(['origin' => $accounts[$origin]]);
         }
-    }elseif($data['type'] === 'transfer'){
-        $origin = $data['orgin'];
+    }elseif ($data['type'] === 'transfer'){
+        $origin = $data['origin'];
         $destination = $data['destination'];
-        $amout = $data['amout'];
-        if(!isset($acconts[$origin]) or !isset($acconts[$destination]) or $acconts[$origin]['balance'] < $amout){
+        $amount = $data['amount'];
+        if(!isset($accounts[$origin]) or !isset($accounts[$destination]) or $accounts[$origin]['balance'] < $amount){
             http_response_code(404);
         } else {
-            $acconts[$origin]['balance'] -= $amout;       
-            $acconts[$destination]['balance'] += $amout;
+            $accounts[$origin]['balance'] -= $amount;       
+            $accounts[$destination]['balance'] += $amount;
             http_response_code(201);
-            echo json_encode(['origin' => $acconts[$origin], 'destination' => $acconts[$destination]]);            
+            echo json_encode(['origin' => $accounts[$origin], 'destination' => $accounts[$destination]]);            
         }
-    } else {
+    }else{
         http_response_code(400);
     }
     exit();
-
 }
+
 http_response_code(404);
