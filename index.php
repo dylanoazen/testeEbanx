6ded5ff1cdf59a4c52c9ab12c18efd1e270d2803
Dylan Oazen <dylanoazen@hotmail.com>
2024-06-01 16:54:45 -0300

    segundo teste

--- index.php
+++ index.php
@@ -1,68 +1,70 @@
 <?php  
+session_start();
 
-$accounts = [];  
+if (!isset($_SESSION['accounts'])) {
+    $_SESSION['accounts'] = [];
+}
 
-if($_SERVER['REQUEST_METHOD'] === 'POST' and $_SERVER['REQUEST_URI'] === '/reset'){
-    $accounts = [];     
-    http_response_code(200);     
-    exit(); 
-}  
+$accounts = &$_SESSION['accounts'];
 
-if($_SERVER['REQUEST_METHOD'] === 'GET' and $_SERVER['REQUEST_URI'] === '/balance'){     
-    $account_id = $_GET['account_id'] ?? null;     
-    if(isset($accounts[$account_id])){         
-        http_response_code(200);         
-        echo json_encode($accounts[$account_id]);     
-    }else{     
-        http_response_code(404);    
-    }     
-    exit(); 
-} 
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/reset') {
+    $_SESSION['accounts'] = [];
+    http_response_code(200);
+    exit();
+}
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' and $_SERVER['REQUEST_URI'] === '/event'){     
-    $data = json_decode(file_get_contents('php://input'), true);     
-    if ($data['type'] === 'deposit') {         
-        $destination = $data['destination'];         
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] === '/balance') {
+    $account_id = $_GET['account_id'] ?? null;
+    if (isset($accounts[$account_id])) {
+        http_response_code(200);
+        echo json_encode($accounts[$account_id]);
+    } else {
+        http_response_code(404);
+    }
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/event') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    if ($data['type'] === 'deposit') {
+        $destination = $data['destination'];
         $amount = $data['amount'];
-        if(!isset($accounts[$destination])) {
+        if (!isset($accounts[$destination])) {
             $accounts[$destination] = ['id' => $destination, 'balance' => $amount];
-        }else {
+        } else {
             $accounts[$destination]['balance'] += $amount;
         }
         http_response_code(201);
         echo json_encode(['destination' => $accounts[$destination]]);
-    }elseif ($data['type'] === 'withdraw') {
+    } elseif ($data['type'] === 'withdraw') {
         $origin = $data['origin'];
         $amount = $data['amount'];
-        if(!isset($accounts[$origin]) or $accounts[$origin]['balance'] < $amount){
+        if (!isset($accounts[$origin]) || $accounts[$origin]['balance'] < $amount) {
             http_response_code(404);
-        }else{
+        } else {
             $accounts[$origin]['balance'] -= $amount;
             http_response_code(201);
             echo json_encode(['origin' => $accounts[$origin]]);
         }
-    }elseif ($data['type'] === 'transfer'){
+    } elseif ($data['type'] === 'transfer') {
         $origin = $data['origin'];
         $destination = $data['destination'];
         $amount = $data['amount'];
-        if(!isset($accounts[$origin]) or !isset($accounts[$destination]) or $accounts[$origin]['balance'] < $amount){
+        if (!isset($accounts[$origin]) || $accounts[$origin]['balance'] < $amount) {
             http_response_code(404);
         } else {
-            $accounts[$origin]['balance'] -= $amount;       
+            if (!isset($accounts[$destination])) {
+                $accounts[$destination] = ['id' => $destination, 'balance' => 0];
+            }
+            $accounts[$origin]['balance'] -= $amount;
             $accounts[$destination]['balance'] += $amount;
             http_response_code(201);
-            echo json_encode(['origin' => $accounts[$origin], 'destination' => $accounts[$destination]]);            
+            echo json_encode(['origin' => $accounts[$origin], 'destination' => $accounts[$destination]]);
         }
-    }else{
+    } else {
         http_response_code(400);
     }
     exit();
 }
 
 http_response_code(404);
-
-
-//Comandos testados com sucesso via curl
-// curl -X POST http://localhost:8000/reset
-// curl -X GET "http://localhost:8000/balance?account_id=1234"
-//curl -X POST -H "Content-Type: application/json" -d '{"type":"deposit", "destination":"100", "amount":10}' http://localhost:8000/event
